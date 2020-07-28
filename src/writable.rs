7cb47ab10c7c547a816ec06f273f82d247f630cd
MinusGix <MinusGix@gmail.com>
2020-07-28 17:46:25 -0500

    Change Writable api to not have a WriteData struct

--- src/writable.rs
+++ src/writable.rs
@@ -13,157 +13,131 @@ impl From<std::io::Error> for WriteError {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct WriteData<D>
-where
-    D: Debug + Clone + PartialEq,
-{
-    pub endian: Endian,
-    pub data: D,
-}
-impl<D> WriteData<D>
-where
-    D: Debug + Clone + PartialEq,
-{
-    pub fn from_endian(endian: Endian, data: D) -> Self {
-        Self { endian, data }
-    }
-
-    pub fn clear(&self) -> WriteData<()> {
-        WriteData::from_empty(self.endian)
-    }
-}
-impl WriteData<()> {
-    pub fn from_empty(endian: Endian) -> Self {
-        Self { endian, data: () }
-    }
-}
-
 // TODO: it'd be nice to support Little|Big endian as a more general crate for my parsing needs
 /// NOTE: all Writables will write integers and floats in little endian.
 pub trait Writable<D>: Sized
 where
     D: Debug + Clone + PartialEq,
 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<D>) -> WriteResult
+    fn write_to<W>(&self, w: &mut W, d: D) -> WriteResult
     where
         W: Write;
 }
 
-impl Writable<()> for u8 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for u8 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for i8 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for i8 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for u16 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for u16 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for i16 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for i16 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for u32 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for u32 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for i32 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for i32 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for u64 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for u64 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for i64 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for i64 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for f32 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for f32 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
         Ok(())
     }
 }
-impl Writable<()> for f64 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<()>) -> WriteResult
+impl Writable<Endian> for f64 {
+    fn write_to<W>(&self, w: &mut W, endian: Endian) -> WriteResult
     where
         W: Write,
     {
-        w.write_all(&match d.endian {
+        w.write_all(&match endian {
             Endian::Big => self.to_be_bytes(),
             Endian::Little => self.to_le_bytes(),
         })?;
@@ -176,12 +150,12 @@ where
     D: Debug + Clone + PartialEq,
 {
     /// Note: does not write length for you, that's up to you.
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<D>) -> WriteResult
+    fn write_to<W>(&self, w: &mut W, d: D) -> WriteResult
     where
         W: Write,
     {
         for entry in self.iter() {
-            entry.write_to(w, d)?;
+            entry.write_to(w, d.clone())?;
         }
         Ok(())
     }
@@ -191,7 +165,7 @@ where
     T: Writable<D>,
     D: Debug + Clone + PartialEq,
 {
-    fn write_to<W>(&self, w: &mut W, d: &mut WriteData<D>) -> WriteResult
+    fn write_to<W>(&self, w: &mut W, d: D) -> WriteResult
     where
         W: Write,
     {
