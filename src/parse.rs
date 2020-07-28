4f555c54e3f60faeaf7446ec9193a8d329365c85
MinusGix <MinusGix@gmail.com>
2020-07-28 17:39:43 -0500

    Change Parse api to not have a ParseData struct

--- src/parse.rs
+++ src/parse.rs
@@ -4,32 +4,6 @@ use std::{
     io::{Read, Seek},
 };
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct ParseData<D>
-where
-    D: Debug + Clone + PartialEq,
-{
-    pub endian: Endian,
-    pub data: D,
-}
-impl<D> ParseData<D>
-where
-    D: Debug + Clone + PartialEq,
-{
-    pub fn from_endian(endian: Endian, data: D) -> Self {
-        Self { endian, data }
-    }
-
-    pub fn clear(&self) -> ParseData<()> {
-        ParseData::from_empty(self.endian)
-    }
-}
-impl ParseData<()> {
-    pub const fn from_empty(endian: Endian) -> ParseData<()> {
-        Self { endian, data: () }
-    }
-}
-
 #[derive(Debug)]
 pub enum ParseError {
     Io(std::io::Error),
@@ -63,7 +37,7 @@ impl<V> From<EnumConversionError<V>> for ParseError {
 
 pub type ParseResult<R, E = ParseError> = Result<R, E>;
 
-pub fn single<F>(f: &mut F, _d: &mut ParseData<()>) -> ParseResult<u8>
+pub fn single<F>(f: &mut F) -> ParseResult<u8>
 where
     F: Read + Seek,
 {
@@ -74,7 +48,7 @@ where
 
 // TODO: const generics version that takes in the size as a template param
 //  and returns an array of that size
-pub fn take<F>(f: &mut F, _d: &mut ParseData<()>, amount: usize) -> ParseResult<Vec<u8>>
+pub fn take<F>(f: &mut F, amount: usize) -> ParseResult<Vec<u8>>
 where
     F: Read + Seek,
 {
@@ -90,10 +64,10 @@ where
 /// Note that this does not rollback when it encounters an error
 /// and should be used when you know that what you're reading from is
 /// parseable by repeated calls to [func]
-pub fn many<F, C, R, E, D>(f: &mut F, d: &mut ParseData<D>, func: C) -> ParseResult<Vec<R>>
+pub fn many<F, C, R, E, D>(f: &mut F, d: D, func: C) -> ParseResult<Vec<R>>
 where
     F: Read + Seek,
-    C: Fn(&mut F, &mut ParseData<D>) -> Result<R, E>,
+    C: Fn(&mut F, D) -> Result<R, E>,
     E: Into<ParseError>,
     D: Debug + Clone + PartialEq,
 {
@@ -104,7 +78,7 @@ where
             break;
         }
 
-        let value: R = match func(f, d) {
+        let value: R = match func(f, d.clone()) {
             Ok(x) => x,
             Err(e) => return Err(e.into()),
         };
@@ -116,7 +90,7 @@ where
     Ok(result)
 }
 
-pub fn many_parse<F, P, D>(f: &mut F, d: &mut ParseData<D>) -> ParseResult<Vec<P>>
+pub fn many_parse<F, P, D>(f: &mut F, d: D) -> ParseResult<Vec<P>>
 where
     F: Read + Seek,
     P: Parse<D>,
@@ -130,7 +104,8 @@ where
             break;
         }
 
-        let value: P = P::parse(f, d)?;
+        // TODO: if we're passed a reference, does this clone the reference or the type behind the reference?
+        let value: P = P::parse(f, d.clone())?;
         result.push(value);
     }
 
@@ -140,13 +115,13 @@ where
 }
 
 /// Expect certain bytes. Does not return them.
-pub fn tag<F, X>(f: &mut F, d: &mut ParseData<()>, data: &[X]) -> ParseResult<()>
+pub fn tag<F, X>(f: &mut F, data: &[X]) -> ParseResult<()>
 where
     F: Read + Seek,
     X: PartialEq<u8>,
 {
     for x in data.iter() {
-        let value = single(f, d)?;
+        let value = single(f)?;
         if x != &value {
             return Err(ParseError::InvalidByte);
         }
@@ -155,30 +130,27 @@ where
 }
 
 // Internal utilities, since const generics don't exist
-fn take_2<F, D>(f: &mut F, _d: &mut ParseData<D>) -> ParseResult<[u8; 2]>
+fn take_2<F>(f: &mut F) -> ParseResult<[u8; 2]>
 where
     F: Read + Seek,
-    D: Debug + Clone + PartialEq,
 {
     let mut output = [0u8; 2];
     f.read_exact(&mut output)?;
     Ok(output)
 }
 
-fn take_4<F, D>(f: &mut F, _d: &mut ParseData<D>) -> ParseResult<[u8; 4]>
+fn take_4<F>(f: &mut F) -> ParseResult<[u8; 4]>
 where
     F: Read + Seek,
-    D: Debug + Clone + PartialEq,
 {
     let mut output = [0u8; 4];
     f.read_exact(&mut output)?;
     Ok(output)
 }
 
-fn take_8<F, D>(f: &mut F, _d: &mut ParseData<D>) -> ParseResult<[u8; 8]>
+fn take_8<F>(f: &mut F) -> ParseResult<[u8; 8]>
 where
     F: Read + Seek,
-    D: Debug + Clone + PartialEq,
 {
     let mut output = [0u8; 8];
     f.read_exact(&mut output)?;
@@ -193,127 +165,127 @@ pub trait Parse<D>: Sized
 where
     D: Debug + Clone + PartialEq,
 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<D>) -> ParseResult<Self>
+    fn parse<F>(f: &mut F, d: D) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek;
 }
 
-impl Parse<()> for u8 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for u8 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = single(f, d)?;
-        Ok(match d.endian {
+        let data = single(f)?;
+        Ok(match endian {
             Endian::Big => u8::from_be_bytes([data]),
             Endian::Little => u8::from_le_bytes([data]),
         })
     }
 }
 
-impl Parse<()> for i8 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for i8 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = single(f, d)?;
-        Ok(match d.endian {
+        let data = single(f)?;
+        Ok(match endian {
             Endian::Big => i8::from_be_bytes([data]),
             Endian::Little => i8::from_le_bytes([data]),
         })
     }
 }
-impl Parse<()> for u16 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for u16 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_2(f, d)?;
-        Ok(match d.endian {
+        let data = take_2(f)?;
+        Ok(match endian {
             Endian::Big => u16::from_be_bytes(data),
             Endian::Little => u16::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for i16 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for i16 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_2(f, d)?;
-        Ok(match d.endian {
+        let data = take_2(f)?;
+        Ok(match endian {
             Endian::Big => i16::from_be_bytes(data),
             Endian::Little => i16::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for u32 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for u32 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_4(f, d)?;
-        Ok(match d.endian {
+        let data = take_4(f)?;
+        Ok(match endian {
             Endian::Big => u32::from_be_bytes(data),
             Endian::Little => u32::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for i32 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for i32 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_4(f, d)?;
-        Ok(match d.endian {
+        let data = take_4(f)?;
+        Ok(match endian {
             Endian::Big => i32::from_be_bytes(data),
             Endian::Little => i32::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for u64 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for u64 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_8(f, d)?;
-        Ok(match d.endian {
+        let data = take_8(f)?;
+        Ok(match endian {
             Endian::Big => u64::from_be_bytes(data),
             Endian::Little => u64::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for i64 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for i64 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_8(f, d)?;
-        Ok(match d.endian {
+        let data = take_8(f)?;
+        Ok(match endian {
             Endian::Big => i64::from_be_bytes(data),
             Endian::Little => i64::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for f32 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for f32 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_4(f, d)?;
-        Ok(match d.endian {
+        let data = take_4(f)?;
+        Ok(match endian {
             Endian::Big => f32::from_be_bytes(data),
             Endian::Little => f32::from_le_bytes(data),
         })
     }
 }
-impl Parse<()> for f64 {
-    fn parse<F>(f: &mut F, d: &mut ParseData<()>) -> ParseResult<Self>
+impl Parse<Endian> for f64 {
+    fn parse<F>(f: &mut F, endian: Endian) -> ParseResult<Self>
     where
         F: std::io::Read + std::io::Seek,
     {
-        let data = take_8(f, d)?;
-        Ok(match d.endian {
+        let data = take_8(f)?;
+        Ok(match endian {
             Endian::Big => f64::from_be_bytes(data),
             Endian::Little => f64::from_le_bytes(data),
         })
@@ -348,7 +320,7 @@ macro_rules! impl_parse_field {
 macro_rules! impl_parse {
     ($on:ty, [$($name:ident : $e:ident : $typ:ty),*]) => {
         impl $crate::parse::Parse<()> for $on {
-            fn parse<F>(f: &mut F, _d: &mut $crate::parse::ParseData<()>) -> $crate::parse::ParseResult<Self>
+            fn parse<F>(f: &mut F, _d: ()) -> $crate::parse::ParseResult<Self>
             where
                 F: std::io::Seek + std::io::Read {
                 $(
@@ -362,7 +334,7 @@ macro_rules! impl_parse {
     };
     (newtype $on:ty, $name:ident : $e:ident : $typ:ty) => {
         impl $crate::parse::Parse<()> for $on {
-            fn parse<F>(f: &mut F, _d: &mut $crate::parse::ParseData<()>) -> $crate::parse::ParseResult<Self>
+            fn parse<F>(f: &mut F, _d: ()) -> $crate::parse::ParseResult<Self>
             where
                 F: std::io::Seek + std::io::Read {
                 $crate::impl_parse_field!($name: $e : $typ; f);
@@ -381,26 +353,25 @@ mod tests {
         0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11,
         0x12, 0x13, 0x14,
     ];
-    const PDATA: ParseData<()> = ParseData::from_empty(Endian::Little);
 
     #[test]
     fn test_single() {
         let mut cursor = Cursor::new(&DATA);
-        assert_eq!(single(&mut cursor, &mut PDATA).unwrap(), 0x1);
-        assert_eq!(single(&mut cursor, &mut PDATA).unwrap(), 0x2);
-        assert_eq!(single(&mut cursor, &mut PDATA).unwrap(), 0x3);
-        assert_eq!(single(&mut cursor, &mut PDATA).unwrap(), 0x4);
+        assert_eq!(single(&mut cursor).unwrap(), 0x1);
+        assert_eq!(single(&mut cursor).unwrap(), 0x2);
+        assert_eq!(single(&mut cursor).unwrap(), 0x3);
+        assert_eq!(single(&mut cursor).unwrap(), 0x4);
     }
 
     #[test]
     fn test_take() {
         let mut cursor = Cursor::new(&DATA);
         assert_eq!(
-            take(&mut cursor, &mut PDATA, 4).unwrap().as_slice(),
+            take(&mut cursor, 4).unwrap().as_slice(),
             &[0x1, 0x2, 0x3, 0x4]
         );
         assert_eq!(
-            take(&mut cursor, &mut PDATA, 4).unwrap().as_slice(),
+            take(&mut cursor, 4).unwrap().as_slice(),
             &[0x5, 0x6, 0x7, 0x8]
         );
     }
@@ -408,18 +379,17 @@ mod tests {
     #[test]
     fn test_tag() {
         let mut cursor = Cursor::new(&DATA);
-        tag(&mut cursor, &mut PDATA, &[0x1, 0x2, 0x3, 0x4]).unwrap();
-        tag(&mut cursor, &mut PDATA, &[0x5, 0x6, 0x7, 0x8]).unwrap();
+        tag(&mut cursor, &[0x1, 0x2, 0x3, 0x4]).unwrap();
+        tag(&mut cursor, &[0x5, 0x6, 0x7, 0x8]).unwrap();
 
-        tag(&mut cursor, &mut PDATA, &[0x20, 0x52])
-            .expect_err("Expected error since invalid bytes!");
+        tag(&mut cursor, &[0x20, 0x52]).expect_err("Expected error since invalid bytes!");
     }
 
     #[test]
     fn test_many() {
         let mut cursor = Cursor::new(&DATA);
-        let result = many(&mut cursor, &mut PDATA, |f, d| -> ParseResult<[u8; 4]> {
-            let data = take(f, d, 4)?;
+        let result = many(&mut cursor, (), |f, _d| -> ParseResult<[u8; 4]> {
+            let data = take(f, 4)?;
             Ok([data[0], data[1], data[2], data[3]])
         })
         .unwrap();
@@ -435,8 +405,8 @@ mod tests {
         );
 
         let mut cursor = Cursor::new(&DATA);
-        many(&mut cursor, &mut PDATA, |f, d| -> ParseResult<[u8; 3]> {
-            let data = take(f, d, 3)?;
+        many(&mut cursor, (), |f, _d| -> ParseResult<[u8; 3]> {
+            let data = take(f, 3)?;
             Ok([data[0], data[1], data[2]])
         })
         .expect_err("Expected failure in dividing DATA into 3-byte chunks");
